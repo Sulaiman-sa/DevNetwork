c90bc72da352afeabd3df94be9cf9af6eefb5cba
ASoulPro <A.Soul@protonmail.com>
2020-02-05 15:07:51 +0300

    cleaner code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const cors = require('cors')
+
 const app = express()
 const PORT = process.env.PORT || 5000
 const path = require('path')
